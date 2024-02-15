835672b0086323f5f63e34723216c798b74ac54f
Christian Dürselen <17407360+christianduerselen@users.noreply.github.com>
2024-02-15 20:31:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SerialRelay
+
+Software tool to relay and delay data between two serial ports.
---
+++ src/SerialRelay/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace SerialRelay;
+
+public static class Program
+{
+    public static void Main()
+    {
+    }
+}
